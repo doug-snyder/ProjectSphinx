0e6c30caa70b7fe270624143ff3ccfe947cfb159
MisterShake2099 <mistershake2099@gmail.com>
2026-08-08 10:04:00 -0400

    Some changes.

--- ProjectSphinx/app/index.tsx
+++ ProjectSphinx/app/index.tsx
@@ -2,8 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
+    <View style={styles.nonSpecified}
+    
+    {{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
