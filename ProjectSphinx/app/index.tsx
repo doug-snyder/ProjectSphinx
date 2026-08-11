b0209f6d8bda3bf6981b9643b16146ae56460590
douglas-snyder <mistershake2099@gmail.com>
2026-08-11 18:45:28 -0400

    Small changes.

--- ProjectSphinx/app/index.tsx
+++ ProjectSphinx/app/index.tsx
@@ -2,14 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.nonSpecified}
+    <View style={styles.nonSpecified}>
     
     {{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
