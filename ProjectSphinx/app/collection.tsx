a75390bbeb778d2bd44dedfad8c45552d1ec67fe
douglas-snyder <mistershake2099@gmail.com>
2026-08-18 20:46:03 -0400

    Add Collection page.

---
+++ ProjectSphinx/app/collection.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Collection() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>COLLECTION</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#9BA1A6",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+    text: {
+    color: "#000000",
+    },
+});
